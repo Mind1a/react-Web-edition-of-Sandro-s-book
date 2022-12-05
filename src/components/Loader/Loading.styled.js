1f65e1cd611eafcef61b286e526c51b3aa69daeb
nikkoloz <nkhuk2019@agruni.edu.ge>
2022-12-05 23:09:59 +0400

    adding framer-motion, styled-components, Loader

---
+++ src/components/Loader/Loading.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+export const SLoader = styled(motion.div)`
+    min-height: 100vh;
+    position: fixed;
+    background-color: gray;
+`;
