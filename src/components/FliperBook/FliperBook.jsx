66a47e76bb3a0b1e6409a9e05e982f4c8e57e712
Mind1a <mindiaarabuli4@gmail.com>
2023-02-18 14:45:20 +0400

    feature/PdfFlipBook

---
+++ src/components/FliperBook/FliperBook.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import HTMLFlipBook from "react-pageflip";
+import styles from "./FliperBook.module.scss";
+
+const padIndex = (i) => String(i).padStart(3, "0");
+
+export const FliperBook = () => {
+  return (
+    <HTMLFlipBook className={styles.flipBook} width={500} height={500}>
+      {[...Array(100)].map((e, i) => {
+        return (
+          <div className={styles.demoPage} key={i}>
+            <img
+              src={`/assets/png/FlipBook/${`SandroAsatiani_ChaosidanCosmosamde-${padIndex(
+                i + 1
+              )}.png`}`}
+              alt={i}
+            />
+          </div>
+        );
+      })}
+    </HTMLFlipBook>
+  );
+};
---
+++ src/components/FliperBook/index.js
@@ -0,0 +1,1 @@
+export { FliperBook } from "./FliperBook";
